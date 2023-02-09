d4ba9175b5fd22183cc4a752b92862e2f2e9ed56
Andres Martinez <amartinezcarrillo1006@gmail.com>
2023-02-09 00:03:21 -0300

    Entrego desafio del procces

--- src/routes/randoms.js
+++ src/routes/randoms.js
@@ -1,5 +1,5 @@
 process.on("message", (msg) => {
-    console.log("Child Process received message", msg);
+    console.log("El proceso hijo recibio un parametro", msg);
     const result = getRandom(msg);
     process.send(result);
   
@@ -8,12 +8,12 @@ process.on("message", (msg) => {
     }, 5000);
   });
   
-  function getRandom(cant) {
+  function getRandom(cantidad) {
     const numeros = [];
-    for (let i = 0; i < cant; i++) {
+    for (let i = 0; i < cantidad; i++) {
       numeros.push(Math.floor(Math.random() * 1000) + 1);
     }
-    //contar cuantas veces salió cada numero
+    
     const contador = numeros.reduce((acc, num) => {
       acc[num] = (acc[num] || 0) + 1;
       return acc;
